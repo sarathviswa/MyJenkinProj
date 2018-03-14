843f81016dfa34582f7e50bded238ded84300b56
sarath viswanath <sarathviswa@yahoo.com>
2018-03-14 22:25:58 +0000

    ms 1

--- src/main/java/sha/com/EclipseMvnTest/App.java
+++ src/main/java/sha/com/EclipseMvnTest/App.java
@@ -8,13 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
+
 
 
         System.out.println( "Hello World123!" );
 
-=======
-        System.out.println( "Hello World1234!" );
->>>>>>> sha1
+
+       
+
     }
 }
